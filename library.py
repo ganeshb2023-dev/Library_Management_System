c1056410daad98bd5c64c2913dd4cb8a8d34deb2
Ganesh B <ganesh.b2023@vitstudent.ac.in>
2026-01-01 17:35:51 +0530

    Branch A change

--- library.py
+++ library.py
@@ -1,5 +1,3 @@
-def login(username, password):
-    if username == "admin" and password == "admin123":
-        print("Login successful")
-    else:
-        print("Invalid credentials")
+print("Branch A update")
+
+
